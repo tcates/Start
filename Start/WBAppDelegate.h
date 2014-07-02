fc347774379194a4c89f97a786a5e6aa563733ac
Tara Cates <tara@tcates.com>
2014-07-02 10:58:57 -0700

    Initial Commit

---
+++ Start/WBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WBAppDelegate.h
+//  Start
+//
+//  Created by Tara Cates on 7/2/14.
+//  Copyright (c) 2014 Tara Cates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Start/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Start
+//
+//  Created by Tara Cates on 7/2/14.
+//  Copyright (c) 2014 Tara Cates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WBAppDelegate class]));
+    }
+}
